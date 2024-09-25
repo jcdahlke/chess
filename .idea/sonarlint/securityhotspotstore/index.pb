698847771d64ec3ec04a9b53ae24367385f1c119
jcdahlke <150458574+jcdahlke@users.noreply.github.com>
2024-09-25 09:20:03 -0600

    Adding of PieceMoveCalculator Class

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,31 +1,9 @@
 
-d
-4src/test/java/passoff/chess/piece/PawnMoveTests.java,6\f\6fe1354a9b6f2adaebc86ce17c85f70c46d74e50
-d
-4src/test/java/passoff/chess/piece/KingMoveTests.java,3\8\38ebd7b9921639dfb1e79eae7f090973e8652e69
-O
-src/main/java/chess/Bishop.java,6\c\6cb815aae8e377af6ccd9b5f3867dc04573f9a80
-O
-src/main/java/chess/Knight.java,8\5\85d09d29ca5a198e68ef91d46063e048fdac751c
-f
-6src/test/java/passoff/chess/piece/BishopMoveTests.java,6\4\647e3d86830d550e7907dcb4f8ca199c4442d021
 S
 #src/main/java/chess/ChessPiece.java,8\5\85a88ea4958ed1b3e2cc2a545a3ad992329e985a
 S
 #src/main/java/chess/ChessBoard.java,1\3\139d0d4bb2b8e11a4e03dbed5b694452387fb52a
-M
-src/main/java/chess/Pawn.java,d\6\d6749b7d66d9cf56c001e712de56c8df6ccabe26
-f
-6src/test/java/passoff/chess/piece/KnightMoveTests.java,e\a\ea923ae5656d777ebbc1ce26adecb7338f8ba7dd
-M
-src/main/java/chess/King.java,a\b\ab2da006d7d4f0b405a25a6baf1b1d4b69ae03dd
-e
-5src/test/java/passoff/chess/piece/QueenMoveTests.java,0\a\0a959e861aa739d9c24b9e29d313101dcb5e8325
-N
-src/main/java/chess/Queen.java,f\8\f869e0cddbe0f50e2e3dcc55fe9efee9f6b24457
-d
-4src/test/java/passoff/chess/piece/RookMoveTests.java,6\9\69e9785f60de8da7a1625ee0a9db7af2bd25e7cb
-M
-src/main/java/chess/Rook.java,5\f\5f60c58ccbf62143e0a9c1c153a79e2e6142738e
 `
 0src/test/java/passoff/chess/ChessBoardTests.java,8\a\8af669845ac3f59bd12b81507de0f5b73d6fe308
+\
+,src/main/java/chess/PieceMoveCalculator.java,6\b\6b3cf47f986a2254ff76a3694291a4d46a65972b
---
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -0,0 +1,81 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PieceMoveCalculator {
+  private final ChessPiece piece;
+  private final ChessBoard board;
+  private final ChessPosition position;
+
+  public PieceMoveCalculator(ChessPiece piece, ChessBoard board, ChessPosition position) {
+
+    this.piece=piece;
+    this.board=board;
+    this.position=position;
+  }
+
+  public Collection<ChessMove> pieceSorter() {
+    if (piece.getPieceType()== ChessPiece.PieceType.BISHOP) {
+      BishopMoveCalculator bishop = new BishopMoveCalculator(piece,board,position);
+      return bishop.possibleMovesCalculator();
+    }
+    else if (piece.getPieceType()== ChessPiece.PieceType.KING) {
+      KingMoveCalculator king = new KingMoveCalculator(piece,board,position);
+      return king.possibleMovesCalculator();
+    }
+    else if (piece.getPieceType()== ChessPiece.PieceType.QUEEN) {
+      QueenMoveCalculator queen = new QueenMoveCalculator(piece,board,position);
+      return queen.possibleMovesCalculator();
+    }
+    else if (piece.getPieceType()== ChessPiece.PieceType.ROOK) {
+      RookMoveCalculator rook = new RookMoveCalculator(piece,board,position);
+      return rook.possibleMovesCalculator();
+    }
+    else if (piece.getPieceType()== ChessPiece.PieceType.KNIGHT) {
+      KnightMoveCalculator knight = new KnightMoveCalculator(piece,board,position);
+      return knight.possibleMovesCalculator();
+    }
+    else if (piece.getPieceType()== ChessPiece.PieceType.PAWN) {
+      PawnMoveCalculator pawn = new PawnMoveCalculator(piece,board,position);
+      return pawn.possibleMovesCalculator();
+    }
+    else{
+      return null;
+    }
+  }
+
+
+  public Collection<ChessMove> directionMover(int rowIncrement, int columnIncrement, ChessPosition startPosition, boolean continuousMovement) {
+    Collection<ChessMove> chessMoveCollection = new ArrayList<>();
+    ChessPosition newPosition = new ChessPosition(startPosition.getRow() + rowIncrement, startPosition.getColumn()+columnIncrement);
+    if (continuousMovement){
+      while (!newPosition.isOutOfBounds() && (!spaceOccupied(newPosition))) {
+        chessMoveCollection.add(new ChessMove(startPosition, newPosition));
+        newPosition = new ChessPosition(newPosition.getRow() + rowIncrement, newPosition.getColumn()+columnIncrement);
+      }
+      if (!newPosition.isOutOfBounds() && (!spaceOccupied(newPosition) || board.getPiece(newPosition).getTeamColor() != piece.getTeamColor())) {
+        chessMoveCollection.add(new ChessMove(startPosition, newPosition));
+      }
+    }
+    else {
+      if (!newPosition.isOutOfBounds()) {
+        if (!spaceOccupied(newPosition) || board.getPiece(newPosition).getTeamColor() != piece.getTeamColor()) {
+          chessMoveCollection.add(new ChessMove(startPosition, newPosition));
+        }
+      }
+    }
+    return chessMoveCollection;
+  }
+
+
+  public boolean spaceOccupied (ChessPosition position) {
+    if (board.getPiece(position) != null) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+}
