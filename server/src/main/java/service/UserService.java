71a31fef26e871b014112e93623f0e77a38bb079
jcdahlke <150458574+jcdahlke@users.noreply.github.com>
2024-10-20 20:24:57 -0600

    Started implementation of register service and logout service

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -4,11 +4,27 @@ import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import model.UserData;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class UserService {
   private final DataAccess dataAccess;
 
   public UserService(DataAccess dataAccess) {
     this.dataAccess=dataAccess;
+  }
+
+  public Collection<String> register(String username, String password, String email) throws DataAccessException {
+    if (dataAccess.getUser(username).equals(null)) {
+      throw new DataAccessException("User already exists");
+    }
+    dataAccess.createUser(username, password, email);
+    String authToken =dataAccess.createAuth(username);
+    Collection<String> result = new ArrayList<>();
+    result.add(username);
+    result.add(authToken);
+
+    return result;
   }
 
   public String userLogin(String username, String password) throws DataAccessException {
@@ -19,5 +35,9 @@ public class UserService {
     else {
       throw new DataAccessException("WrongPassword");
     }
+  }
+
+  public void logout(String authToken) throws DataAccessException {
+    dataAccess.deleteAuth(authToken);
   }
 }
