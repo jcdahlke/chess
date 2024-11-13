1b7d844ef24b95b944e7f4c8b40814ff3a65605f
jcdahlke <150458574+jcdahlke@users.noreply.github.com>
2024-11-12 21:28:11 -0700

    Started implementation of PostLoginClient

--- client/src/main/java/ui/PostLoginClient.java
+++ client/src/main/java/ui/PostLoginClient.java
@@ -2,6 +2,8 @@ package ui;
 
 import server.ServerFacade;
 
+import java.util.Arrays;
+
 public class PostLoginClient implements ClientInterface{
   private final ServerFacade serverFacade;
   private final String authToken;
@@ -13,12 +15,54 @@ public class PostLoginClient implements ClientInterface{
 
   @Override
   public String eval(String input) {
+    try {
+      var tokens = input.toLowerCase().split(" ");
+      var cmd = (tokens.length > 0) ? tokens[0] : "help";
+      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      return switch (cmd) {
+        case "create" -> createGame(params);
+        case "list" -> listGames();
+        case "play" -> joinGame(params);
+        case "observe" -> observeGame(params);
+        case "logout" -> logout();
+        case "quit" -> "quit";
+        default -> help();
+      };
+    } catch (Exception ex) {
+      return ex.getMessage();
+    }
+  }
+
+  public String createGame(String... params) {
+    return null;
+  }
+
+  public String listGames() {
+    return null;
+  }
+
+  public String joinGame(String... params) {
+    return null;
+  }
+
+  public String observeGame(String... params) {
+    return null;
+  }
+
+  public String logout() {
     return null;
   }
 
   @Override
   public String help() {
-    return null;
+    return"""
+          - create <gameName>
+          - list
+          - play <gameID> [WHITE|BLACK]
+          - observe <gameID>
+          - logout
+          - quit
+          """;
   }
 
   @Override
